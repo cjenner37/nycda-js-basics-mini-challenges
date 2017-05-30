81dac158654ae2dc65866c960171ed518173c306
Cameron Jenner <cjenner37@gmail.com>
2017-05-30 17:16:09 -0600

    done better

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 /*
+1.
 Create a script with two variables, each assigned to a number. Add them together and output the result to the 
 console. Now do the same with two strings.
 */
@@ -10,6 +11,7 @@ var string1 = "I love ";
 var string2 = "eating donuts!";
 console.log(string1 + string2);
 /*
+2.
 Create a multidimensional array related to a subject that interests you. Output two of the items in sub-arrays 
 to the console.
 */
@@ -22,42 +24,52 @@ var interests = [sports, instruments, foods];
 console.log(interests[1][3]);
 console.log(interests[2][0]);
 /*
+3.
 Write a script that checks if a variable captured from a prompt is less than 100. If it is, alert the user that 
 their variable is less than 100. If it is not alert the user of what the value was and that it was greater than 100.
 */
-var prompt = 101;
+var your_number = prompt("Please enter a number.")
 
-if (prompt < 100) {
+if (your_number < 100) {
 	alert("It's less than 100!");
 } else {
-	alert("It's more than 100, it's " + prompt);
-}
-
-var prompt = 99;
-
-if (prompt < 100) {
-	alert("It's less than 100!");
-} else {
-	alert("It's more than 100, it's " + prompt);
+	alert("It's more than 100, you chose " + your_number);
 }
 /*
+4.
 Try running the script and then changing the variable's value to see how this affects the program's output.
-Write a similar script to check if a string stored in a variable is the same as another string.
 */
-var test_string = "Sonntheen?"
-if ( test_string === "Sonntheen?" ){
+var test_variable = "4"
+if ( test_variable === 4 ){
+	alert("it's the same!");
+} else {
+	alert("your variables are different :(");
+}
+
+
+if ( test_variable === "4" ){
 	alert("it's the same!");
 } else {
 	alert("your strings are different :(");
 }
-
+/*
+5.
+Write a similar script to check if a string stored in a variable is the same as another string.
+*/
 var test_string = "Sonntheen?"
 if ( test_string === "Something?" ){
 	alert("it's the same!");
 } else {
 	alert("your strings are different :(");
 }
+
+if ( test_string === "Sonntheen?" ){
+	alert("it's the same!");
+} else {
+	alert("your strings are different :(");
+}
 /*
+6.
 Declare a function that takes a name as an argument and tells the user what name they've entered. Try running it 
 after it has been declared.
 */
@@ -67,6 +79,7 @@ function echo(name){
 
 echo("Jeff");
 /*
+6.
 Declare a function that takes no arguments but prints something to the console. Try running it after it has been 
 declared.
 */
